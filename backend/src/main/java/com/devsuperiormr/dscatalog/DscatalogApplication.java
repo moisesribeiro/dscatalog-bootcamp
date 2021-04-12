3fcbf72174031ea081283e48d42b52f4c98a3b00
moisesribeiro <moises.rsjr@gmail.com>
2021-04-12 13:28:56 -0300

    Project Created

---
+++ backend/src/main/java/com/devsuperiormr/dscatalog/DscatalogApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperiormr.dscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperiormr/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperiormr.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
