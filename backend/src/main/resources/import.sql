339f85168d79a9abdc71db11bcea78b1b0fc0bda
moisesribeiro <moises.rsjr@gmail.com>
2021-04-16 21:38:24 -0300

    Database seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into tb_category (name) values ('Books');
+insert into tb_category (name) values ('Mouse');
+insert into tb_category (name) values ('Eletronics');
+insert into tb_category (name) values ('Smartphone');
