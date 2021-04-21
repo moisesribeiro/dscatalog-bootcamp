5f6790b56a7d446f15160a923cbf5838b9a45054
moisesribeiro <moises.rsjr@gmail.com>
2021-04-21 15:55:20 -0300

    Auditing data

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,4 +1,4 @@
-insert into tb_category (name) values ('Books');
-insert into tb_category (name) values ('Mouse');
-insert into tb_category (name) values ('Eletronics');
-insert into tb_category (name) values ('Smartphone');
+insert into tb_category (name, created_At) values ('Books', NOW());
+insert into tb_category (name, created_At) values ('Mouse', NOW());
+insert into tb_category (name, created_At) values ('Eletronics', NOW());
+insert into tb_category (name, created_At) values ('Smartphone', NOW());
